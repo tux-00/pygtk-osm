61e707bb78aae504c1871e4624c4da3b4aba6f39
Raph <raphb.bis@gmail.com>
2013-10-31 19:49:16 +0100

    Reorganized code

--- src/pygtk_osm_game.py
+++ src/pygtk_osm_game.py
@@ -18,9 +18,12 @@
 #
 
 from gi.repository import Gtk, GtkChamplain, GtkClutter, Champlain, Clutter
-from urllib.error import URLError
 
-import sys, urllib.request, json, ast
+import sys
+import json
+import ast
+import urllib.request
+from urllib.error import URLError
 
 
 UI_FILE = "./pygtk_osm_game.ui"
@@ -72,7 +75,6 @@ class GUI:
 		
 		window.show_all()
 
-
 	def on_button_search_clicked(self, widget):
 		
 		# Get search request from entry_search
@@ -81,7 +83,7 @@ class GUI:
 		# Get data from Nominatim
 		data = self.request_json_data(to_search)
 		
-		if data == False: 
+		if data is False: 
 			return False
 
 		# Center the result and zoom it
@@ -94,7 +96,7 @@ class GUI:
 			self.multi_layer[i].remove_all()
 		
 		# Trace polygons
-		if self.is_highlight == True:
+		if self.is_highlight:
 			self.trace_polygons(data['geojson']['coordinates'],
 								len(data['geojson']['coordinates']),
 								data['geojson']['type'])
@@ -104,8 +106,8 @@ class GUI:
 							float(data['lon']),
 							self.get_zoom_by_type(data['type']))
 
-
 	def get_zoom_by_type(self, result_type=None):
+		# TODO: Optimize
 
 		zoom = 10
 
@@ -115,20 +117,21 @@ class GUI:
 		
 		return zoom
 
-
 	def trace_polygons(self, points, multipoints=0, coord_type=None):
-		
+		# TODO: Thread
+			
 		# Stroke and fill RGB color
 		stroke_color = Clutter.Color.new(11, 191, 222, 255)
 		fill_color = Clutter.Color.new(255, 255, 255, 150)
 
 		if multipoints != 0 and coord_type == 'MultiPolygon':
-			# FIXME: Bug when double click on search button
 			for i in range(0, multipoints):
 				self.multi_layer.append(Champlain.PathLayer())
 				
 				for j in range(0, len(points[i][0])):
-					coord = Champlain.Coordinate.new_full(float(points[i][0][j][1]), float(points[i][0][j][0]))
+					coord = Champlain.Coordinate.new_full(
+													float(points[i][0][j][1]),
+													float(points[i][0][j][0]))
 					self.multi_layer[i].add_node(coord)
 				
 				self.multi_layer[i].set_stroke_color(stroke_color)
@@ -142,7 +145,8 @@ class GUI:
 				
 		elif coord_type == 'Polygon':
 			for i in range(0, len(points[0])):
-				coord = Champlain.Coordinate.new_full(float(points[0][i][1]), float(points[0][i][0]))
+				coord = Champlain.Coordinate.new_full(float(points[0][i][1]),
+													float(points[0][i][0]))
 				self.polygon_layer.add_node(coord)
 
 			self.polygon_layer.set_stroke_color(stroke_color)
@@ -160,8 +164,8 @@ class GUI:
 		# Connect marker click signal
 		# marker.connect("button-release-event", action, self.map_view)
 
-
 	def create_marker(self, map_view, lat, lon, zoom, label=None):
+		# TODO: Thread
 		# TODO: Get icons on JSON data ?
 		
 		marker = Champlain.Label.new_from_file(MARKER_IMG_PATH)
@@ -184,7 +188,6 @@ class GUI:
 			self.map_view.set_property('zoom-level', zoom)
 
 		self.marker_layer.raise_top()
-
 
 	def request_json_data(self, to_search):
 
@@ -210,7 +213,8 @@ class GUI:
 		
 		# If search not found
 		if(len(data) == 0):
-			self.new_error_dialog("Search \'" + str(to_search) + "\' not found.")
+			self.new_error_dialog("Search \'" + str(to_search) 
+								+ "\' not found.")
 			return False
 		
 		# Make dictionary
@@ -224,14 +228,12 @@ class GUI:
 		
 		return data
 
-
 	def new_error_dialog(self, label):
 		if(isinstance(label, str)):
 			self.error_dialog.format_secondary_text(label)
 			self.error_dialog.run()
 			self.error_dialog.hide()
 		else: return False
-		
 		
 	def highlight_search(self, is_highlight):
 		self.polygon_layer.set_visible(is_highlight)
@@ -240,19 +242,17 @@ class GUI:
 			for i in range(0, len(self.multi_layer)):
 				self.multi_layer[i].set_visible(is_highlight)
 
-
 	def on_entry_search_icon_press(self, *args):
 		self.entry_search.set_text('')
 		self.button_search.grab_focus()
-		
 		
 	def on_highlight_item_toggled(self, highlight_item):
 		self.highlight_search(highlight_item.get_active())
 		self.is_highlight = highlight_item.get_active()
 		
-
 	def destroy(self, window):
 		Gtk.main_quit()
+
 
 def main():
 	GUI()
